78630cc5ac5443244a50f5d95b55763b7aedb5a0
Minh Hoang <minh.hoang@tcu.edu>
2015-05-12 20:17:14 +0700

    completed the computing of minkowski distance

--- src/distance/MinkowskiDistance.java
+++ src/distance/MinkowskiDistance.java
@@ -8,6 +8,13 @@ public class MinkowskiDistance {
 		
 	}
 
+	/*
+	 * Compute the distance between 2 people. The smaller the distance, they have more similarities.
+	 * @param the mode to compute the distance. 1 to compute Manhattan Distance. 2 to compute Euclidean Distance.
+	 * @param the first person to compare. This should be a HashMap with the key is the name of the product and the map is the rating.
+	 * @param the second person.
+	 * @return the distance between the 2 provided people. 
+	 */
 	public double computerMinkowski(int r, Map<String, Double> firstPerson, Map<String, Double> secondPerson) {	
 		float distance = 0;
 		for (String key: firstPerson.keySet()) {
--- src/distance/MinkowskiDistanceTest.java
+++ src/distance/MinkowskiDistanceTest.java
@@ -10,7 +10,13 @@ public class MinkowskiDistanceTest {
 	
 	MinkowskiDistance distance = new MinkowskiDistance();
 	Map<String, Double> angelica = new HashMap<String, Double>();
-	
+	Map<String, Double> bill = new HashMap<String, Double>();
+	Map<String, Double> chan = new HashMap<String, Double>();
+	Map<String, Double> dan = new HashMap<String, Double>();
+	Map<String, Double> hailey = new HashMap<String, Double>();
+	Map<String, Double> jordyn = new HashMap<String, Double>();
+	Map<String, Double> sam = new HashMap<String, Double>();
+	Map<String, Double> veronica = new HashMap<String, Double>();
 	
 	public void initMap() {
 		angelica.put("Blues Traveler", 3.5);
@@ -20,10 +26,64 @@ public class MinkowskiDistanceTest {
 		angelica.put("Slightly Stoopid", 1.5);
 		angelica.put("The Strokes", 2.5);
 		angelica.put("Campire Weekend", 2.0);
+		
+		bill.put("Blues Traveler", 2.0);
+		bill.put("Broken Bells", 3.5);
+		bill.put("Deadmau5", 4.0);
+		bill.put("Phoenix", 2.0);
+		bill.put("Slightly Stoopid", 3.5);
+		bill.put("Campire Weekend", 3.0);
+		
+		chan.put("Blues Traveler", 5.0);
+		chan.put("Broken Bells", 1.0);
+		chan.put("Deadmau5", 1.0);
+		chan.put("Noral Jones", 3.0);
+		chan.put("Phoenix", 5.0);
+		chan.put("Slightly Stoopid", 1.0);
+		
+		dan.put("Blues Traveler", 3.0);
+		dan.put("Broken Bells", 4.0);
+		dan.put("Deadmau5", 4.5);
+		dan.put("Phoenix", 3.0);
+		dan.put("Slightly Stoopid", 4.5);
+		dan.put("The Strokes", 4.0);
+		dan.put("Campire Weekend", 2.0);
+		
+		hailey.put("Broken Bells", 4.0);
+		hailey.put("Deadmau5", 1.0);
+		hailey.put("Noral Jones", 4.0);
+		hailey.put("The Strokes", 4.0);
+		hailey.put("Campire Weekend", 1.0);
+		
+		jordyn.put("Broken Bells", 4.5);
+		jordyn.put("Deadmau5", 4.0);
+		jordyn.put("Noral Jones", 5.0);
+		jordyn.put("Phoenix", 5.0);
+		jordyn.put("Slightly Stoopid", 4.5);
+		jordyn.put("The Strokes", 4.0);
+		jordyn.put("Campire Weekend", 4.0);
+		
+		sam.put("Blues Traveler", 5.0);
+		sam.put("Broken Bells", 2.0);
+		sam.put("Noral Jones", 3.0);
+		sam.put("Phoenix", 5.0);
+		sam.put("Slightly Stoopid", 4.0);
+		sam.put("The Strokes", 5.0);
+		
+		veronica.put("Blues Traveler", 3.0);
+		veronica.put("Noral Jones", 5.0);
+		veronica.put("Phoenix", 4.0);
+		veronica.put("Slightly Stoopid", 2.5);
+		veronica.put("The Strokes", 3.0);
 	}
 
 	@Test
 	public void computerMinkowski() {
+		initMap();
+		double d = distance.computerMinkowski(1, hailey, veronica);
+		assertEquals(2.0, d, 0.001);
+		d = distance.computerMinkowski(1, hailey, jordyn);
+		assertEquals(7.5, d, 0.001);
 	}
 
 }
