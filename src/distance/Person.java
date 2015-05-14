620d65accaf186a42f91756e1d726caac4c902f8
Minh Hoang <minh.hoang@tcu.edu>
2015-05-14 14:09:34 +0700

    Improved person class for the project

--- src/distance/Person.java
+++ src/distance/Person.java
@@ -1,14 +1,48 @@
 package distance;
-
+import java.util.HashMap;
 import java.util.Map;
 
 public class Person {
 	private String name;
+	private int id;
+	private String location;
+	private int age;
 	private Map<String, Double> rating;
 	
 	public Person(String name, Map<String, Double> rating) {
 		this.name = name;
 		this.rating = rating;
+	}
+	
+	public Person(int id, String location, int age) {
+		this.id = id;
+		this.name = null;
+		this.location = location;
+		this.age = age;
+		rating = new HashMap<>();
+	}
+	
+	public Person(int id, String location) {
+		this.id = id;
+		this.name = null;
+		this.location = location;
+		rating = new HashMap<>();
+	}
+	
+	public void putRating(String isbn, Double score) {
+		rating.put(isbn, score);
+	}
+	
+	public int getID() {
+		return id;
+	}
+	
+	public String getLocation() {
+		return location;
+	}
+	
+	public int getAge() {
+		return age;
 	}
 	
 	public String getName() {
@@ -22,5 +56,4 @@ public class Person {
 	public String toString() {
 		return name;
 	}
-	
 }
