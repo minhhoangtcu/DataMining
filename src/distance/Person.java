140b2052ed25525922d5ad68b52f9acc34cf0d4d
minhhoangtcu <minh.hoang@tcu.edu>
2015-05-19 23:25:28 -0500

    Added some features for the Person class to support movies

--- src/distance/Person.java
+++ src/distance/Person.java
@@ -7,11 +7,12 @@ public class Person {
 	private int id;
 	private String location;
 	private int age;
-	private Map<String, Double> rating;
+	private Map<String, Double> bookRating;
+	private Map<Integer, Double> movieRating;
 	
 	public Person(String name, Map<String, Double> rating) {
 		this.name = name;
-		this.rating = rating;
+		this.bookRating = rating;
 		this.location = null;
 		this.age = -1;
 		this.id = -1;
@@ -22,7 +23,7 @@ public class Person {
 		this.name = null;
 		this.location = location;
 		this.age = age;
-		rating = new HashMap<>();
+		bookRating = new HashMap<>();
 	}
 	
 	public Person(int id, String location) {
@@ -30,7 +31,7 @@ public class Person {
 		this.name = null;
 		this.location = location;
 		this.age = -1;
-		rating = new HashMap<>();
+		bookRating = new HashMap<>();
 	}
 	
 	public Person(int id) {
@@ -38,11 +39,15 @@ public class Person {
 		this.name = null;
 		this.location = null;
 		this.age = -1;
-		rating = new HashMap<>();
+		bookRating = new HashMap<>();
 	}
 	
 	public void putRating(String productID, Double score) {
-		rating.put(productID, score);
+		bookRating.put(productID, score);
+	}
+	
+	public void putRating(int productID, Double score) {
+		movieRating.put(productID, score);
 	}
 	
 	public int getID() {
@@ -61,8 +66,12 @@ public class Person {
 		return name;
 	}
 	
-	public Map<String, Double> getRating() {
-		return rating;
+	public Map<String, Double> getBookRating() {
+		return bookRating;
+	}
+	
+	public Map<Integer, Double> getMovieRating() {
+		return movieRating;
 	}
 
 	public String toString() {
