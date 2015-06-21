d61e5941e107d8b7988e96aef4f23f517eff9a82
Minh Hoang <minh.hoang@tcu.edu>
2015-06-21 22:34:53 +0700

    Updated R Health Care practice

--- R/PoorCare.R
+++ R/PoorCare.R
@@ -3,6 +3,8 @@ quality = read.csv("dataset/quality.csv")
 str(quality)
 install.packages("caTools")
 library(caTools)
+install.packages("ROCR")
+library(ROCR)
 
 set.seed(88)
 split = sample.split(quality$PoorCare, SplitRatio = 0.75)
@@ -10,4 +12,10 @@ qualityTrain = subset(quality, split == TRUE)
 qualityTest = subset(quality, split == FALSE)
 
 QualityLog = glm(PoorCare ~ StartedOnCombination + ProviderCount, data=qualityTrain, family=binomial)
+QualityLog = glm(PoorCare ~ OfficeVisits + Narcotics, data=qualityTrain, family=binomial)
+predictTest = predict(QualityLog, type="response", newdata=qualityTest)
 summary(QualityLog) 
+
+ROCRpredTest = prediction(predictTest, qualityTest$PoorCare)
+auc = as.numeric(performance(ROCRpredTest, "auc")@y.values)
+auc
