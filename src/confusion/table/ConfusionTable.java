610f9f62555def145f031c9e9a6a3308b0ab7a3c
Minh Hoang <minh.hoang@tcu.edu>
2015-07-13 20:19:05 +0700

    Fixed when 2 input String have different classification

--- src/confusion/table/ConfusionTable.java
+++ src/confusion/table/ConfusionTable.java
@@ -3,6 +3,7 @@ package confusion.table;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.stream.Stream;
 
 public class ConfusionTable {
 	
@@ -13,17 +14,20 @@ public class ConfusionTable {
 	}
 	
 	public static void printTable(String[] actual, String[] predict) {
+		
 		String[] uniqueActual = new HashSet<String>(Arrays.asList(actual)).toArray(new String[0]); // We will use this as a general list of unique classifications
 		String[] uniquePredict = new HashSet<String>(Arrays.asList(predict)).toArray(new String[0]);
-		if ((!Arrays.equals(uniqueActual, uniquePredict)) | (uniqueActual.length != uniquePredict.length) | (actual.length != predict.length)) {
+		String[] combinedClassifications = Stream.concat(Arrays.stream(uniqueActual), Arrays.stream(uniquePredict)).toArray(String[]::new);
+		String[] unique = new HashSet<String>(Arrays.asList(combinedClassifications)).toArray(new String[0]);
+		if ((actual.length != predict.length)) {
 			System.err.println("Actual and predict array must have same length or classifications.");
 			throw new IllegalArgumentException("Actual and predict array must have same length or classifications.");
 		}
 		else {
 			// Initialize confusion matrix with value of 0
 			HashMap<String,Integer> cmatrix = new HashMap<String,Integer>();
-			for (String actualClassName : uniqueActual) {
-				for (String predictedClassName : uniqueActual) {
+			for (String actualClassName : unique) {
+				for (String predictedClassName : unique) {
 					String key = actualClassName + "|" + predictedClassName; // We use only 1 list of unique
 					cmatrix.put(key, 0);
 				}
@@ -40,14 +44,14 @@ public class ConfusionTable {
 			
 			// Print confusion matrix
 			System.out.print("p/a");
-			for(String predictedClassName : uniqueActual) {
+			for(String predictedClassName : unique) {
 			    System.out.print("\t" + predictedClassName);
 			}
 			System.out.println();
 			
-			for(String actualClassName : uniqueActual) {
+			for(String actualClassName : unique) {
 			    System.out.print(actualClassName);
-			    for(String predictedClassName : uniqueActual) {
+			    for(String predictedClassName : unique) {
 			        Integer value = cmatrix.get(actualClassName + "|" + predictedClassName);
 			        System.out.print("\t");
 			        if(value != null) {
