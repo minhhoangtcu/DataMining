b5fc4a64316422aaf6edbc13b74a580ca10190f4
Minh Hoang <minh.hoang@tcu.edu>
2015-07-13 23:18:50 +0700

    Kaggle - Let all female lives except for some.

--- R/Titanic.R
+++ R/Titanic.R
@@ -1,12 +1,31 @@
 # Set working directory and import datafiles
 setwd("D:/Computer Science/R/Dataset")
-TitanicTrain = read.csv("D:/Computer Science/R/Dataset/TitanicTrain.csv")
-TitanicTest = read.csv("D:/Computer Science/R/Dataset/TitanicTest.csv")
-str(TitanicTrain)
+train = read.csv("D:/Computer Science/R/Dataset/TitanicTrain.csv")
+test = read.csv("D:/Computer Science/R/Dataset/TitanicTest.csv")
+str(train)
 
 # Inspect the train set, make baseline model and output an csv file of people' survival predicted using the model.
-prop.table(table(TitanicTrain$Survived)) # We see that most people died
-TitanicTest$Survived = 0
-submit = data.frame(PassengerId = TitanicTest$PassengerId, Survived = TitanicTest$Survived)
+prop.table(table(train$Survived)) # We see that most people died
+test$Survived = 0
+submit = data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
 write.csv(submit, file = "theyallperish.csv", row.names = FALSE)
 
+# Women and Children often go on the boat first. Thus, we inspect the survival rate for genders.
+prop.table(table(train$Sex, train$Survived),1)
+test$Survived[test$Sex == 'female'] = 1
+summary(train$Age)
+train$Child = 0
+train$Child[train$Age < 18] = 1
+aggregate(Survived ~ Child + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+
+# Inspect the fare
+train$Fare2 = '30+'
+train$Fare2[train$Fare < 30 & train$Fare >= 20] = '20-30'
+train$Fare2[train$Fare < 20 & train$Fare >= 10] = '10-20'
+train$Fare2[train$Fare < 10] = '<10'
+aggregate(Survived ~ Fare2 + Pclass + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+test$Survived = 0
+test$Survived[test$Sex == 'female'] = 1
+test$Survived[test$Sex == 'female' & test$Pclass == 3 & test$Fare >= 20] = 0
+submit = data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
+write.csv(submit, file = "Titanic01.csv", row.names = FALSE)
